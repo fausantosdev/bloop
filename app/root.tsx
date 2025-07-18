81bb7e4b241a3b8e9d8ebdbb9d1943fe99573b55
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2025-07-18 15:15:39 -0300

    feat: create Welcome page for initial app entry

--- app/root.tsx
+++ app/root.tsx
@@ -25,10 +25,11 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="./favicon.png"></link>
         <Meta />
         <Links />
       </head>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home"
+
+import bloop from "../assets/bloop.webp"
+import { Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +11,30 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+        <main className="flex items-center justify-center pt-16 pb-4 px-5">
+      <div className="flex-1 flex flex-col items-center gap-5 max-w-[700px]">
+        <img 
+          src={bloop} alt="Bloop, mascote da aplicação"
+          className="w-32"/>
+        <h1 className="text-5xl text-bloop-primary-dark mt-0">Bloop</h1>
+        <p className="text-gray-600 text-center">
+          Bem-vindo ao Bloop!  
+          Compartilhe momentos, ideias ou o que quiser — e descubra o que o mundo tem a dizer.  
+          Curta, comente, interaja.
+        </p>
+        <div className="w-[100%] flex flex-col md:flex-row gap-5">
+          <Link to="/" className="flex-1 text-center bg-bloop-primary p-3 rounded-md text-white">
+            Entrar
+          </Link>
+          <Link to="/" className="flex-1 text-center bg-bloop-primary-light p-3 rounded-md text-gray-800">
+            Criar conta
+          </Link>
+          <Link to="/" className="flex-1 text-center bg-gray-100 p-3 rounded-md text-bloop-primary-dark border border-bloop-primary-dark">
+            Explorar como visitante
+          </Link>
+        </div>
+      </div>
+    </main>
+  )
 }
